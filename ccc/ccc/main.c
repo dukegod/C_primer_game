a490603bcba969ec94ff3936f531c93fd06023d5
dukegod <dukehui1991@gmail.com>
2017-07-24 21:36:44 +0800

    c

---
+++ ccc/ccc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ccc
+//
+//  Created by hui on 2017/7/24.
+//  Copyright © 2017年 hui. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ ccc/ccc/variable.c
@@ -0,0 +1,31 @@
+//
+//  variable.c
+//  ccc
+//
+//  Created by hui on 2017/7/24.
+//  Copyright © 2017年 hui. All rights reserved.
+//
+
+#include <stdio.h>
+
+extern int a;
+extern int b;
+
+int main(){
+    /* 变量定义 */
+    int a, b;
+    int c;
+    float f;
+    
+    /* 初始化 */
+    a = 10;
+    b = 20;
+    
+    c = a + b;
+    printf("value of c : %d \n", c);
+    
+    f = 70.0/3.0;
+    printf("value of f : %f \n", f);
+    
+    return 0;
+}
