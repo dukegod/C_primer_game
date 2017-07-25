2c94e2b723d001f01e566a8f53d3cfc0f3eae6d8
dukegod <dukehui1991@gmail.com>
2017-07-25 18:17:01 +0800

    debug

---
+++ cmake-build-debug/CMakeFiles/C_primer_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_primer_game.dir/main.c.o"
+  "C_primer_game.pdb"
+  "C_primer_game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_primer_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
